7d4d0233bc26b04b52d6e633118949f945dee90d
Marco Leogrande <dark.knight.ita@gmail.com>
2021-01-28 21:27:29 -0800

    Add parsing of airspaces

--- xml2kml.py
+++ xml2kml.py
@@ -18,12 +18,78 @@
 #
 
 import argparse
-import xml.etree.ElementTree as ET
+import attr
+from typing import List
+from xml.etree import ElementTree
+
+# Constant namespaces used in all XML files. See XML file headers.
+NAMESPACES = {
+    "ns2": "http://www.w3.org/1999/xlink",
+    "ns1": "http://www.opengis.net/gml/3.2",
+    "ns4": "http://www.isotc211.org/2005/gco",
+    "ns3": "http://www.aixm.aero/schema/5.1",
+    "ns5": "http://www.isotc211.org/2005/gmx",
+    "ns6": "http://www.isotc211.org/2005/gmd",
+    "ns7": "http://www.isotc211.org/2005/gts",
+    "ns8": "http://www.aixm.aero/schema/5.1/message",
+}
+
+
+@attr.s
+class Position:
+    lat = attr.ib()
+    long = attr.ib()
+
+
+class Airspace:
+    """Class representing a chunk of airspace in the MVA chart."""
+
+    # Name of the airspace
+    name: str = ...
+
+    # The airspace floor, in ft MSL
+    floor: int = ...
+
+    # The vertexes of the airspace
+    vertexes: List[Position] = ...
+
+    def __init__(self, element: ElementTree.Element):
+        self.name = element.find("ns3:name", NAMESPACES).text
+        volume = element.find(
+            "ns3:geometryComponent/ns3:AirspaceGeometryComponent/ns3:theAirspaceVolume/ns3:AirspaceVolume",
+            NAMESPACES,
+        )
+        self.floor = volume.find("ns3:minimumLimit", NAMESPACES).text
+        coords = volume.find(
+            "ns3:horizontalProjection/ns3:Surface/ns1:patches/ns1:PolygonPatch/ns1:exterior/ns1:LinearRing/ns1:posList",
+            NAMESPACES,
+        ).text
+        # Coords is a long string of longitude/latitude pairs
+        # separated by spaces.
+        coords = coords.split(" ")
+        if len(coords) % 2 != 0:
+            raise ValueError(
+                "Cannot parse element with odd number of coordinate values: {}".format(
+                    len(coords)
+                )
+            )
+        self.vertexes = []
+        for i in range(0, len(coords), 2):
+            self.vertexes.append(
+                Position(lat=float(coords[i + 1]), long=float(coords[i]))
+            )
 
 
 class Chart:
-    def parse_xml(filename: str) -> "Chart":
-        return Chart()
+    def __init__(self, filename: str):
+        tree = ElementTree.parse(filename)
+        root = tree.getroot()
+
+        self.airspaces = []
+        for airspace in root.findall(
+            "ns8:hasMember/ns3:Airspace/ns3:timeSlice/ns3:AirspaceTimeSlice", NAMESPACES
+        ):
+            self.airspaces.append(Airspace(airspace))
 
     def write_kml(self, filename: str):
         pass
@@ -32,10 +98,11 @@ class Chart:
 def main(input: str, output: str) -> None:
     print("Converting input file %s to output %s" % (input, output))
     print("Parsing input file...")
-    chart = Chart.parse_xml(input)
-    print("Input file parsed successfully")
+    chart = Chart(input)
+    print("Input file with %d airspaces parsed successfully" % len(chart.airspaces))
     chart.write_kml(output)
     print("Output file written successfully")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
