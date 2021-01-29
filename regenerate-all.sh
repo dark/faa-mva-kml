41e537bf1dc225b9013a7e30303f69135c19c5b1
Marco Leogrande <dark.knight.ita@gmail.com>
2021-01-28 23:04:02 -0800

    Update regeneration script

--- regenerate-all.sh
+++ regenerate-all.sh
@@ -1,7 +1,18 @@
-#!/bin/bash
+#!/bin/bash -e
 
-find faa-xml/ -name '*.xml' | while read input; do
-  output="output-kml/$(basename "${input}" | sed 's/\.xml//').kml"
-  echo "${input} -> ${output}"
-  ./xml2kml.py "${input}" -o "${output}"
+D=$(readlink -f "$0" | xargs dirname)
+TMPDIR=$(mktemp -d)
+
+# Regenerate all KML files first.
+mkdir -p "${TMPDIR}/kml/"
+find "${D}/faa-xml/" -name '*.xml' | while read input; do
+  output="${TMPDIR}/kml/$(basename "${input}" | sed 's/\.xml//').kml"
+  echo "  ${input} -> ${output}"
+  "${D}/xml2kml.py" "${input}" -o "${output}"
 done
+
+# Updates all files in place.
+echo
+echo 'Moving files in place...'
+rm -rf "${D}/kml/"
+mv "${TMPDIR}/kml/" "${D}/kml/"
